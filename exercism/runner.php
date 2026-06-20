642d80c24340c9aca766a43a9a09a51a85e59e6d
Julio Davila <jcds1982@gmail.com>
2026-06-20 07:08:50 -0400

    Adding mPDF and some other files like php info and
    I'm adding mPDF library and some other files like
    php info and some other practice files.

--- exercism/runner.php
+++ exercism/runner.php
@@ -17,3 +17,6 @@ $lasagna = new Lasagna();
 echo "Remaining cook time: " . $lasagna->remainingCookTime(30) . " minutes \n";
 echo "Total Preparation Time: " . $lasagna->totalPreparationTime(3) . " minutes \n";
 echo "Total elapse Time: " . $lasagna->totalElapsedTime(4, 13) . " minutes \n";
+
+
+
